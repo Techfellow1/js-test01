d6d3bb331e631dbec359518ba4850e69831bc970
Techfellow1 <109914755+Techfellow1@users.noreply.github.com>
2022-10-02 16:42:29 +0400

    upload

---
+++ script.js
@@ -0,0 +1,106 @@
+// Homework 1.
+
+
+// let user = {
+//     firstname: "giorgi",
+//     lastname: "smith",
+//     age: 25,
+//     studentstatus: "active"
+// };
+
+// console.log(user.studentstatus);
+
+
+
+// Homework 2.
+
+// let scientists = ['Einstein', 'Galileo', 'Newton', 'Tesla', 'Darwin', 'Mendeleev'];
+// for (let x = 0; x < scientists.length; x ++) {    
+//    console.log(scientists[x]);
+// }
+// let i = 0;
+// while (i <scientists.length-3) {
+// console.log(scientists[i]);
+// i++;
+// }
+
+
+// Homework 3.
+
+// let numbers = [5,6,10,25,3,7,4,100,28,36,89];
+// for (let x = 0; x <numbers.length; x ++) {    
+//    if (numbers [x]>5) 
+// console.log(numbers[x]);
+// }
+// // x++ : x = x + 1
+
+
+// Homework 4.
+
+// let user = {
+//    name: 'giorgi',
+//    age: '20',
+//    studentstatus: 'active'
+// }
+
+// if (user.age>18) {
+// console.log('hello');
+// }
+// else if (user.name='levan') {
+// console.log ('Hello levan');
+// }
+// else if (user.studentstatus.active || user.age>25) {
+//    console.log('hello world'); 
+// } 
+// else {
+// console.log ('error')
+// }
+
+
+// Homework 5.
+
+// let array = ['watermelon', 'pear', 10, 45, 50, 'apple', 'ananas'];
+// for (let item of array) {
+//   if (typeof item == 'string') {
+//     console.log(item);
+//   }
+// }
+
+
+// Homework 6.
+
+// let array = [[2, -3, 5, 10], [25,-24, 11, 100], [-6, -7, 10]];
+// for (let x of array) {
+//   for (let positive of x) {
+//     if (positive > 0) {
+//       console.log(positive);
+//     }
+//   }
+// }
+
+    
+   
+
+// // Homework 7.
+
+// let array = [2,3,5,10,25,24,11,100,6,7,10]
+//   for (item of array) {
+//     if (item % 2 == 0) {
+//      console.log(item);
+//     };
+// }
+
+
+// // Homework 8.
+
+// let users = [
+//   {username: 'giorgi', status: false},
+//   {username: 'levani', status: false},
+//   {username: 'anna', status: true}
+// ]
+
+// for (let item of users) {
+//   if (item.status == true) {
+//     console.log(item);
+//   }
+// }
